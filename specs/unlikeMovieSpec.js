22981f3ba20c1f23917a78bbb167e3610d2b1d0e
abgvin <muhamadapri10@gmail.com>
2020-11-03 00:07:50 +0700

    Test unlike Button

---
+++ specs/unlikeMovieSpec.js
@@ -0,0 +1,71 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteMovieIdb from '../src/scripts/data/favorite-movie-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('Unliking A Movie', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteMovieIdb.putMovie({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteMovieIdb.deleteMovie(1);
+  });
+
+  it('Should display unlike widget when the movie has been like', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this movie"]'))
+      .toBeTruthy();
+  });
+
+  it('Should not display like widget when the movie has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this movie"]'))
+      .toBeFalsy();
+  });
+
+  it('Should be able to remove liked movie from list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id : 1,
+      },
+    });
+
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([]);
+  });
+
+  it('Should not throw error if the unlike movie is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      movie: {
+        id: 1,
+      },
+    });
+
+    // HAPUS FILM DARI FILM YANG DISUKAI
+    await FavoriteMovieIdb.deleteMovie(1);
+
+    // SIMULASIKAN PENGGUNA MENEKAN TOMBOL WIDGET BATAL MENYUKAI FILM
+    document.querySelector('[aria-label="unlike this movie"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteMovieIdb.getAllMovies()).toEqual([]);
+  });
+});
